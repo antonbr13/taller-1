aeb0fd2dfc5ccb6a2317a2026bd87c40c833e678
Gabriel Cabrejas Poole <Gabriel@Gabriels-MacBook-Air.local>
2015-07-18 00:22:25 -0600

    fixes numeration

--- taller-1.js
+++ taller-1.js
@@ -1,6 +1,6 @@
 // I- WHAT IS YOUR TYPE?
 
-// loop through the array types, console.logging the type of each element:
+// 1- loop through the array types, console.logging the type of each element:
 // Use the typeof keyword
 // typeof 7; => "number"
 
@@ -14,7 +14,7 @@ for (var i = 0; i < types.length; i += 1) {
 
 // II- WHAT IS YOUR BOOLEAN?
 
-// 1- loop through the falsies array, console.logging the boolean value of each element:
+// 2- loop through the falsies array, console.logging the boolean value of each element:
 // Use the double bang !!
 // !!6; => true
 // !!""; => false
@@ -29,8 +29,8 @@ for (var i = 0; i < falsies.length; i += 1) {
 
 // III- THE WINDOW OBJECT
 
-// 2- declare a variable x in chrome console and bind it to the number 5
-// 3- type window.x; in the console
+// 3- declare a variable x in chrome console and bind it to the number 5
+// type window.x; in the console
 // x is now a property of the window object!
 
 
@@ -67,18 +67,18 @@ var b = a;
 a += 1;
 
 
-//9.1- to what eveluates yy after these three lines of code? {a:1} or {a:1, b:2}? Why?
+// 10- to what eveluates yy after these three lines of code? {a:1} or {a:1, b:2}? Why?
 var xx = { a: 1 };
 var yy = xx;
 xx.b = 2;
 
 
-// 10-  To what value evaluates d? 3 or 4? Why?
+// 11-  To what value evaluates d? 3 or 4? Why?
 var c = 3;
 var d = c++;
 
 
-// 11-  To what value evaluates f? 5 or 6? Why?
+// 12-  To what value evaluates f? 5 or 6? Why?
 var e = 5;
 var f = ++e;
 
@@ -86,31 +86,31 @@ var f = ++e;
 
 // VI- EXPRESSIONS
 
-// 12- What is the value of foo? Why?
+// 13- What is the value of foo? Why?
 var foo = function () {
     return "hello!";
 };
 
 
-// 13- What is the value of bar? Why?
+// 14- What is the value of bar? Why?
 var bar = function () {
     return "hello!";
 }();
 
 
-// 14- What is the value of baz? Why?
+// 15- What is the value of baz? Why?
 var baz = function (name) {
     "hello, " + name;
 };
 
 
-// 15- What is the value of buzz? Why?
+// 16- What is the value of buzz? Why?
 var buzz = function (name) {
     "hello, " + name;
 }("Wendy");
 
 
-// 16- Which is the function declaration and which the function expression?
+// 17- Which is the function declaration and which the function expression?
 var someFoo = function () {
     return 8;
 };
@@ -123,7 +123,7 @@ function someOtherFoo() {
 
 // VII- LOGICAL EXPRESSIONS, TRUTHY AND FALSY VALUES
 
-// 17- What do the following expressions evaluate to?
+// 18- What do the following expressions evaluate to?
 1 > 2; //a
 4 || true; //b
 "" || true; //c
@@ -139,66 +139,66 @@ false || !false; //g
 (3 > 6) || (NaN === NaN); //m
 
 
-// 18- How do you provide the variable myVar with a fallback/default value of "default"?
+// 19- How do you provide the variable myVar with a fallback/default value of "default"?
 
 
 
 // VIII- OBJECTS
 
 
-// 19- Loop through the following object console.logging the NAMES of its properties using a for/in loop:
+// 20- Loop through the following object console.logging the NAMES of its properties using a for/in loop:
 var o = { a: 1, b: "hello", c: [1, 2, 3], d: function () { return "goodbye!"; } };
 
 
-// 20- Loop through the following object console.logging the VALUES of its properties using a for/in loop:
+// 21- Loop through the following object console.logging the VALUES of its properties using a for/in loop:
 var p = { a: 1, b: "hello", c: [1, 2, 3], d: function () { return "goodbye!"; } };
 
 
-// 21- Add a property "email" with a value "myemail@gmail.com" to the following object:
+// 22- Add a property "email" with a value "myemail@gmail.com" to the following object:
 var erik = { name: "Erik", age: 19 };
 
 
-// 22- Add to erik the property "happy with his code" with a value of true
+// 23- Add to erik the property "happy with his code" with a value of true
 
 
-// 23- Test erik to check whether he HAS OR INHERITS the property "name""
+// 24- Test erik to check whether he HAS OR INHERITS the property "name""
 // For this test you use the in operator
 // "x" in { x:1, y:2, z:3 } => true
 
 
-// 24- Test erik to check whether he HAS OR INHERITS the method "toString"
+// 25- Test erik to check whether he HAS OR INHERITS the method "toString"
 
 
-// 25- Test erik to check whether he HAS, AS OWN, the property "email"
+// 26- Test erik to check whether he HAS, AS OWN, the property "email"
 // For this test you use the hasOwnProperty() method
 // { x:1, y:2, z:3 }.hasOwnProperty("x") => true
 
 
-// 25- Test erik to check whether he HAS, AS OWN, the method "toString"
+// 27- Test erik to check whether he HAS, AS OWN, the method "toString"
 
 
 
 // IX- ARRAYS
 
-// 26- Iterate through the following array console.logging only the falsy values:
+// 28- Iterate through the following array console.logging only the falsy values:
 var myArray = [" ", 4, true, null, NaN, "0", 2015];
 
 
-// 27- Concatenate the following arrays using .concatenate()
+// 29- Concatenate the following arrays using .concatenate()
 var aa = [1, 2], bb = [3, 4], cc = [5, 6];
 
 
-// 28- Join into a string the elements of the following array:
+// 30- Join into a string the elements of the following array:
 var hi = ["h", "e", "l", "l", "o"];
 
 
-// 29- Slice [1,2,3,4,5] so that you obtain the new array [2,3,4]
+// 31- Slice [1,2,3,4,5] so that you obtain the new array [2,3,4]
 
 
-// 30- Splice [1,2,3,4,5] so that you mutate the array into [1,"2","3","4",5]
+// 32- Splice [1,2,3,4,5] so that you mutate the array into [1,"2","3","4",5]
 
 
-// 31- Is .pop() destructive or not? What are its side effects? What does it return?
+// 33- Is .pop() destructive or not? What are its side effects? What does it return?
 // Is .push() destructive or not? What are its side effects? What does it return?
 // Is .shift() destructive or not? What are its side effects? What does it return?
 // Is .unshift() destructive or not? What are its side effects? What does it return?
@@ -207,33 +207,33 @@ var hi = ["h", "e", "l", "l", "o"];
 
 // X- HIGHER ORDER FUNCTIONS
 
-// 31- Create a function sumSeven, which takes an integer, and RETURNS the sum of the argument and 7
+// 34- Create a function sumSeven, which takes an integer, and RETURNS the sum of the argument and 7
 
 
-// 32- Pass sumSeven as an argument to the following invocation and see what it returns:
+// 35- Pass sumSeven as an argument to the following invocation and see what it returns:
 // Does it modify the array in place, or does it return a new array?
 [10, 100, 1000, 10000].map(/* pass me a function */);
 
 
-// 33- Using .map() square all the numbers in [1,2,3,4,5]
+// 36- Using .map() square all the numbers in [1,2,3,4,5]
 
 
-// 34- Create a function isEven, which takes an integer, and RETURNS true if the integer is even, false otherwise
+// 37- Create a function isEven, which takes an integer, and RETURNS true if the integer is even, false otherwise
 
 
-// 35- Pass isEven as an argument to the following invocation and see what it returns:
+// 38- Pass isEven as an argument to the following invocation and see what it returns:
 // Does it modify the array in place, or does it return a new array?
 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].filter(/* pass me a function */);
 
 
-// 36- Using .filter() obtain a new array with the values in [1, 25, 3, 14, 10] that are greater than 10
+// 39- Using .filter() obtain a new array with the values in [1, 25, 3, 14, 10] that are greater than 10
 
 
-// 37- Create a function mult, which takes two integers, and RETURNS the numbers multiplied by each other
+// 40- Create a function mult, which takes two integers, and RETURNS the numbers multiplied by each other
 
 
-// 38- Pass mult as an argument to the following invocation and see what it returns:
+// 41- Pass mult as an argument to the following invocation and see what it returns:
 [1, 2, 3, 4, 5].reduce(/* pass me a function */);
 
 
-// 39- Using .reduce() obtain the sum of all the values in [1,3,5,7,9]
+// 42- Using .reduce() obtain the sum of all the values in [1,3,5,7,9]
